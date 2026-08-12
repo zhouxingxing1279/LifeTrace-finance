e16d2fb4b95de26a58611fae72e01c94d8daa279
zhouxingxing1279 <76243394+zhouxingxing1279@users.noreply.github.com>
2026-08-12 17:18:06 +0800

    test(ui): import Compose tag collection matcher

--- app/src/androidTest/java/com/lifetrace/finance/LaunchTest.kt
+++ app/src/androidTest/java/com/lifetrace/finance/LaunchTest.kt
@@ -4,6 +4,7 @@ import android.os.SystemClock
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
