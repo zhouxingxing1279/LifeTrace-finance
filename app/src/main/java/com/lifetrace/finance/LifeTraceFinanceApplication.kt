09dd98f8b9878e849e94dc21d65827e2db88b3d0
zhouxingxing1279 <76243394+zhouxingxing1279@users.noreply.github.com>
2026-08-12 11:03:10 +0800

    Restore screenshot monitor at app startup

--- app/src/main/java/com/lifetrace/finance/LifeTraceFinanceApplication.kt
+++ app/src/main/java/com/lifetrace/finance/LifeTraceFinanceApplication.kt
@@ -2,4 +2,9 @@ package com.lifetrace.finance
 
 import android.app.Application
 
-class LifeTraceFinanceApplication : Application()
+class LifeTraceFinanceApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppGraph.get(this).screenshotMonitor.restore()
+    }
+}
